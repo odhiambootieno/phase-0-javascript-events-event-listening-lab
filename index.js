108df4c0ac9e85e1cf3f3f2ffb1fc9bfa374c415
odhiambootieno <brianmkcnight@gmail.com>
2024-12-27 10:34:47 +0300

    modified

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', () => {
+        console.log('Button clicked!');
+    });
 }
+
+// Call the function to bind the event listener
+addingEventListener();
